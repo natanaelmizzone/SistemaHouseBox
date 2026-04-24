16306c6496ca9b11150926f89a3ae928e5281195
Natanael Mizzone <natanmizzone@gmail.com>
2026-04-24 19:47:58 -0300

    Classe Cliente
    - Classe cliente adicionado e variáveis realizadas.

---
+++ sistema_box_house/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sistema_box_house
+
+{
+	public class Cliente
+	{
+		public string NomeCliente (get; set;)
+		public int PhoneTelefone(get; set;)
+		public string NomePet(get; set;)
+
+		public Cliente(string nomeCliente, int telefone, string nomePet)
+		{
+			NomeCliente = nomeCliente;
+			PhoneTelefone = telefone;
+			NomePet = nomePet;
+
+		}
+		public void fnMsgAddCliente()
+		{
+
+		}
+		
+	}
+}
